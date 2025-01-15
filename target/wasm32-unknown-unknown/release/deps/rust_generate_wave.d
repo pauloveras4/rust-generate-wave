2074b58decf481d274fc24a8d5abaabf804abfb5
atarax <atarax@localhost.localdomain>
2025-01-14 22:28:42 -0300

    Adding pkg.

---
+++ target/wasm32-unknown-unknown/release/deps/rust_generate_wave.d
@@ -0,0 +1,7 @@
+/home/atarax/Projetos/darcy-music/wasm-generate-wave/target/wasm32-unknown-unknown/release/deps/rust_generate_wave.wasm: src/lib.rs
+
+/home/atarax/Projetos/darcy-music/wasm-generate-wave/target/wasm32-unknown-unknown/release/deps/librust_generate_wave.rlib: src/lib.rs
+
+/home/atarax/Projetos/darcy-music/wasm-generate-wave/target/wasm32-unknown-unknown/release/deps/rust_generate_wave.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/wasm32-unknown-unknown/release/rust_generate_wave.d
@@ -0,0 +1,1 @@
+/home/atarax/Projetos/darcy-music/wasm-generate-wave/target/wasm32-unknown-unknown/release/rust_generate_wave.wasm: /home/atarax/Projetos/darcy-music/wasm-generate-wave/src/lib.rs
